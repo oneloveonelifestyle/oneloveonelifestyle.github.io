291f9ff6740de523cb1880de3dd94e12e1ba5b3d
One Love <royaltrendzindia@gmail.com>
2026-01-29 09:18:50 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,6 +1,7 @@
-/* =========================
-   MASTER PRODUCT DATABASE
-   ========================= */
+/* ================================
+   CENTRAL PRODUCT DATABASE
+   DO NOT PUT UI LOGIC HERE
+================================ */
 
 const PRODUCTS = [
   /* ================= CLOTHING ================= */
@@ -10,13 +11,14 @@ const PRODUCTS = [
     category: "clothing",
     title: "Campus Sutra Zip-Front Bomber Jacket",
     price: "₹1,999",
+    status: "instock",
     short: "Regular Fit",
-    search: "campus sutra jacket bomber regular",
+    search: "campus sutra bomber jacket regular",
     images: ["jacket1-1.jpg", "jacket1-2.jpg", "jacket1-3.jpg"],
     description: `
-      <b>Size :</b> S, M, L, XL<br><br>
+      <b>Size:</b> S, M, L, XL<br><br>
       <b>Fit:</b> Regular Fit<br>
-      <b>Care Instruction:</b> Machine Wash
+      <b>Care:</b> Machine Wash
     `
   },
 
@@ -25,15 +27,15 @@ const PRODUCTS = [
     category: "clothing",
     title: "Nuon Brown Text Design Relaxed-Fit Cotton Jacket",
     price: "₹2,999",
+    status: "instock",
     short: "Relaxed Fit",
-    search: "nuon jacket cotton relaxed brown",
+    search: "nuon cotton jacket brown relaxed",
     images: ["shirt1-3.jpg", "shirt1-4.jpg", "shirt1-5.jpg"],
     description: `
-      <b>Size :</b> XS, S, M, L, XL<br><br>
+      <b>Size:</b> XS, S, M, L, XL<br><br>
       <b>Fit:</b> Relaxed Fit<br>
-      <b>Care Instruction:</b> Machine Wash<br>
-      <b>Fabric Composition:</b> 100% Cotton<br><br>
-      <b>Country Of Origin:</b> India
+      <b>Fabric:</b> 100% Cotton<br>
+      <b>Country:</b> India
     `
   },
 
@@ -42,14 +44,14 @@ const PRODUCTS = [
     category: "clothing",
     title: "ETA Brown Textured Relaxed-Fit Cotton Shirt",
     price: "₹1,799",
+    status: "instock",
     short: "Relaxed Fit",
-    search: "eta shirt cotton brown relaxed",
+    search: "eta brown shirt cotton relaxed",
     images: ["shirt1-1.jpg", "shirt1-2.jpg"],
     description: `
-      <b>Size :</b> XS, S, M, L<br><br>
+      <b>Size:</b> XS, S, M, L<br><br>
       <b>Fit:</b> Relaxed Fit<br>
-      <b>Care Instruction:</b> Machine Wash<br>
-      <b>Fabric Composition:</b> 100% Cotton
+      <b>Fabric:</b> 100% Cotton
     `
   },
 
@@ -58,13 +60,14 @@ const PRODUCTS = [
     category: "clothing",
     title: "Studiofit Dark Brown Relaxed-Fit Hooded Jacket",
     price: "₹2,199",
+    status: "instock",
     short: "Relaxed Fit",
-    search: "studiofit dark brown hooded jacket relaxed",
+    search: "studiofit hooded jacket brown relaxed",
     images: ["jacket3-1.jpg", "jacket3-2.jpg", "jacket3-3.jpg"],
     description: `
-      <b>Size :</b> XS, S, M, L<br><br>
+      <b>Size:</b> XS, S, M, L<br><br>
       <b>Fit:</b> Relaxed Fit<br>
-      <b>Care Instruction:</b> Machine Wash
+      <b>Care:</b> Machine Wash
     `
   },
 
@@ -75,28 +78,33 @@ const PRODUCTS = [
     category: "shoes",
     title: "Men’s Brown Suede Sneakers",
     price: "Currently Out of Stock",
-    short: "Leather Upper",
+    status: "outofstock",
+    short: "Suede Leather",
     search: "mens brown suede sneakers leather",
-    outOfStock: true,
     images: ["shoe1-1.jpg", "shoe1-2.jpg", "shoe1-3.jpg"],
     description: `
-      <b>Size:</b> 6, 7, 8, 9, 10<br>
-      <b>Material:</b> Leather
+      <b>Size:</b> 6, 7, 8, 9, 10<br><br>
+      <b>Material:</b> Suede Leather
     `
   },
 
   {
     id: "shoe-2",
     category: "shoes",
-    title: "Olive Suede Rowan Leather",
-    price: "₹3,599",
-    short: "Premium Leather",
-    search: "olive suede rowan leather shoes",
-    outOfStock: false,
-    images: ["shoe2-1.jpg", "shoe2-2.jpg"],
+    title: "Men’s Olive Suede Sneakers",
+    price: "Currently Out of Stock",
+    status: "outofstock",
+    short: "Olive Suede",
+    search: "mens olive suede sneakers",
+    images: ["shoe2-1.jpg", "shoe2-2.jpg", "shoe2-3.jpg"],
     description: `
-      <b>Material:</b> Suede Leather<br>
-      <b>Color:</b> Olive
+      <b>Size:</b> 6, 7, 8, 9, 10<br><br>
+      <b>Material:</b> Suede Leather
     `
   }
 ];
+
+/* ================================
+   DO NOT EXPORT / MODIFY
+   render-products.js will consume this
+================================ */
