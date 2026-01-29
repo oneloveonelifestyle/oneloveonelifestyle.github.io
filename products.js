30cab56661de5c0779311a6d6c77342b3816af90
One Love <royaltrendzindia@gmail.com>
2026-01-29 10:19:08 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,7 +1,4 @@
 const PRODUCTS = [
-
-  /* ===================== CLOTHING ===================== */
-
   {
     id: "jacket-1",
     category: "clothing",
@@ -10,93 +7,55 @@ const PRODUCTS = [
     short: "Regular Fit",
     search: "campus sutra jacket bomber regular",
     images: ["jacket1-1.jpg", "jacket1-2.jpg", "jacket1-3.jpg"],
-    outOfStock: false,
     description: `
-      <b>Size:</b> S, M, L, XL<br><br>
+      <b>Size :</b> S, M, L, XL<br><br>
       <b>Fit:</b> Regular Fit<br>
-      <b>Care:</b> Machine Wash
+      <b>Care Instruction:</b> Machine Wash
     `
   },
-
   {
     id: "jacket-2",
     category: "clothing",
-    title: "Nuon Brown Relaxed-Fit Cotton Jacket",
+    title: "Nuon Brown Text Design Relaxed-Fit Cotton Jacket",
     price: "₹2,999",
     short: "Relaxed Fit",
-    search: "nuon brown cotton jacket relaxed",
-    images: ["jacket2-1.jpg", "jacket2-2.jpg"],
-    outOfStock: false,
+    search: "nuon jacket cotton relaxed brown",
+    images: ["shirt1-3.jpg", "shirt1-4.jpg", "shirt1-5.jpg"],
     description: `
-      <b>Size:</b> XS, S, M, L, XL<br><br>
+      <b>Size :</b> XS, S, M, L, XL<br><br>
       <b>Fit:</b> Relaxed Fit<br>
-      <b>Fabric:</b> 100% Cotton<br>
-      <b>Care:</b> Machine Wash
+      <b>Care Instruction:</b> Machine Wash<br>
+      <b>Fabric Composition:</b> 100% Cotton<br><br>
+      <b>Country Of Origin:</b> India
     `
   },
-
   {
     id: "shirt-1",
     category: "clothing",
-    title: "ETA Brown Textured Cotton Shirt",
+    title: "ETA Brown Textured Relaxed-Fit Cotton Shirt",
     price: "₹1,799",
     short: "Relaxed Fit",
-    search: "eta brown cotton shirt relaxed",
+    search: "eta shirt cotton brown relaxed",
     images: ["shirt1-1.jpg", "shirt1-2.jpg"],
-    outOfStock: false,
     description: `
-      <b>Size:</b> XS, S, M, L<br><br>
+      <b>Size :</b> XS, S, M, L<br><br>
       <b>Fit:</b> Relaxed Fit<br>
-      <b>Fabric:</b> 100% Cotton
+      <b>Care Instruction:</b> Machine Wash<br>
+      <b>Fabric Composition:</b> 100% Cotton
     `
   },
-
   {
-    id: "jacket-3",
+    id: "jacket-4",
     category: "clothing",
-    title: "Studiofit Dark Brown Hooded Jacket",
+    title: "Studiofit Dark Brown Relaxed-Fit Hooded Jacket",
     price: "₹2,199",
     short: "Relaxed Fit",
-    search: "studiofit dark brown hooded jacket",
+    search: "studiofit dark brown hooded jacket relaxed",
     images: ["jacket3-1.jpg", "jacket3-2.jpg", "jacket3-3.jpg"],
-    outOfStock: false,
     description: `
-      <b>Size:</b> XS, S, M, L<br><br>
+      <b>Size :</b> XS, S, M, L<br><br>
       <b>Fit:</b> Relaxed Fit<br>
-      <b>Care:</b> Machine Wash
-    `
-  },
-
-  /* ===================== SHOES ===================== */
-
-  {
-    id: "shoe-1",
-    category: "shoes",
-    title: "Men’s Brown Suede Sneakers",
-    price: "Currently Out of Stock",
-    short: "Leather Upper",
-    search: "mens brown suede sneakers leather",
-    images: ["shoe1-1.jpg", "shoe1-2.jpg", "shoe1-3.jpg"],
-    outOfStock: true,
-    description: `
-      <b>Size:</b> 6, 7, 8, 9, 10<br><br>
-      <b>Material:</b> Suede Leather
-    `
-  },
-
-  {
-    id: "shoe-2",
-    category: "shoes",
-    title: "Olive Suede Lace-Up Sneakers",
-    price: "₹3,499",
-    short: "Suede Finish",
-    search: "olive suede sneakers lace up",
-    images: ["shoe2-1.jpg", "shoe2-2.jpg"],
-    outOfStock: false,
-    description: `
-      <b>Size:</b> 6, 7, 8, 9, 10<br><br>
-      <b>Material:</b> Suede Leather
+      <b>Care Instruction:</b> Machine Wash
     `
   }
-
 ];
