1fd22a6286a7d6554c9febe75ef4ffd7e8d6e3d2
One Love <royaltrendzindia@gmail.com>
2026-01-29 10:25:56 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,61 +1,51 @@
 const PRODUCTS = [
   {
     id: "jacket-1",
-    category: "clothing",
     title: "Campus Sutra Zip-Front Bomber Jacket",
-    price: "₹1,999",
-    short: "Regular Fit",
-    search: "campus sutra jacket bomber regular",
-    images: ["jacket1-1.jpg", "jacket1-2.jpg", "jacket1-3.jpg"],
-    description: `
-      <b>Size :</b> S, M, L, XL<br><br>
-      <b>Fit:</b> Regular Fit<br>
-      <b>Care Instruction:</b> Machine Wash
-    `
+    category: "clothing",
+    images: [
+      "jacket1-1.jpg",
+      "jacket1-2.jpg",
+      "jacket1-3.jpg"
+    ]
   },
   {
     id: "jacket-2",
-    category: "clothing",
     title: "Nuon Brown Text Design Relaxed-Fit Cotton Jacket",
-    price: "₹2,999",
-    short: "Relaxed Fit",
-    search: "nuon jacket cotton relaxed brown",
-    images: ["shirt1-3.jpg", "shirt1-4.jpg", "shirt1-5.jpg"],
-    description: `
-      <b>Size :</b> XS, S, M, L, XL<br><br>
-      <b>Fit:</b> Relaxed Fit<br>
-      <b>Care Instruction:</b> Machine Wash<br>
-      <b>Fabric Composition:</b> 100% Cotton<br><br>
-      <b>Country Of Origin:</b> India
-    `
-  },
-  {
-    id: "shirt-1",
     category: "clothing",
-    title: "ETA Brown Textured Relaxed-Fit Cotton Shirt",
-    price: "₹1,799",
-    short: "Relaxed Fit",
-    search: "eta shirt cotton brown relaxed",
-    images: ["shirt1-1.jpg", "shirt1-2.jpg"],
-    description: `
-      <b>Size :</b> XS, S, M, L<br><br>
-      <b>Fit:</b> Relaxed Fit<br>
-      <b>Care Instruction:</b> Machine Wash<br>
-      <b>Fabric Composition:</b> 100% Cotton
-    `
+    images: [
+      "shirt1-3.jpg",
+      "shirt1-4.jpg",
+      "shirt1-5.jpg"
+    ]
   },
   {
     id: "jacket-4",
-    category: "clothing",
     title: "Studiofit Dark Brown Relaxed-Fit Hooded Jacket",
-    price: "₹2,199",
-    short: "Relaxed Fit",
-    search: "studiofit dark brown hooded jacket relaxed",
-    images: ["jacket3-1.jpg", "jacket3-2.jpg", "jacket3-3.jpg"],
-    description: `
-      <b>Size :</b> XS, S, M, L<br><br>
-      <b>Fit:</b> Relaxed Fit<br>
-      <b>Care Instruction:</b> Machine Wash
-    `
+    category: "clothing",
+    images: [
+      "jacket3-1.jpg",
+      "jacket3-2.jpg",
+      "jacket3-3.jpg"
+    ]
+  },
+  {
+    id: "shirt-1",
+    title: "ETA Brown Textured Relaxed-Fit Cotton Shirt",
+    category: "clothing",
+    images: [
+      "shirt1-1.jpg",
+      "shirt1-2.jpg"
+    ]
+  },
+  {
+    id: "shoe-1",
+    title: "Men’s Brown Suede Sneakers",
+    category: "shoes",
+    images: [
+      "shoe1-1.jpg",
+      "shoe1-2.jpg",
+      "shoe1-3.jpg"
+    ]
   }
 ];
