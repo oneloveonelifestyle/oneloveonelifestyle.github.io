aa78527c0b0471ea0dfde965a0c127fbef7770c7
One Love <royaltrendzindia@gmail.com>
2026-01-29 07:40:02 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,6 +1,5 @@
 const PRODUCTS = [
-  /* ================= CLOTHING ================= */
-
+  /* ========= CLOTHING ========= */
   {
     id: "jacket-1",
     category: "clothing",
@@ -8,7 +7,6 @@ const PRODUCTS = [
     price: "₹1,999",
     short: "Regular Fit",
     search: "campus sutra jacket bomber regular",
-    page: "clothing.html",
     images: ["jacket1-1.jpg","jacket1-2.jpg","jacket1-3.jpg"],
     description: `
       <b>Size :</b> S, M, L, XL<br><br>
@@ -16,7 +14,6 @@ const PRODUCTS = [
       <b>Care Instruction:</b> Machine Wash
     `
   },
-
   {
     id: "jacket-2",
     category: "clothing",
@@ -24,17 +21,13 @@ const PRODUCTS = [
     price: "₹2,999",
     short: "Relaxed Fit",
     search: "nuon jacket cotton relaxed brown",
-    page: "clothing.html",
     images: ["shirt1-3.jpg","shirt1-4.jpg","shirt1-5.jpg"],
     description: `
       <b>Size :</b> XS, S, M, L, XL<br><br>
       <b>Fit:</b> Relaxed Fit<br>
-      <b>Care Instruction:</b> Machine Wash<br>
-      <b>Fabric Composition:</b> 100% Cotton<br><br>
-      <b>Country Of Origin:</b> India
+      <b>Fabric:</b> 100% Cotton
     `
   },
-
   {
     id: "shirt-1",
     category: "clothing",
@@ -42,17 +35,12 @@ const PRODUCTS = [
     price: "₹1,799",
     short: "Relaxed Fit",
     search: "eta shirt cotton brown relaxed",
-    page: "clothing.html",
     images: ["shirt1-1.jpg","shirt1-2.jpg"],
     description: `
       <b>Size :</b> XS, S, M, L<br><br>
-      <b>Fit:</b> Relaxed Fit<br>
-      <b>Care Instruction:</b> Machine Wash<br>
-      <b>Fabric Composition:</b> 100% Cotton<br><br>
-      <b>Country Of Origin:</b> India
+      <b>Fabric:</b> 100% Cotton
     `
   },
-
   {
     id: "jacket-4",
     category: "clothing",
@@ -60,19 +48,14 @@ const PRODUCTS = [
     price: "₹2,199",
     short: "Relaxed Fit",
     search: "studiofit dark brown hooded jacket relaxed",
-    page: "clothing.html",
     images: ["jacket3-1.jpg","jacket3-2.jpg","jacket3-3.jpg"],
     description: `
       <b>Size :</b> XS, S, M, L<br><br>
-      <b>Fit:</b> Relaxed Fit<br>
-      <b>Care Instruction:</b> Machine Wash<br>
-      <b>Fabric Composition:</b> 79% Polyester, 18% Viscose, 3% Elastane<br><br>
-      <b>Country Of Origin:</b> India
+      <b>Fabric:</b> Polyester Blend
     `
   },
 
-  /* ================= SHOES ================= */
-
+  /* ========= SHOES ========= */
   {
     id: "shoe-1",
     category: "shoes",
@@ -80,23 +63,10 @@ const PRODUCTS = [
     price: "Currently Out of Stock",
     short: "",
     search: "mens brown suede sneakers leather",
-    page: "shoes.html",
     images: ["shoe1-1.jpg","shoe1-2.jpg","shoe1-3.jpg"],
     description: `
       <b>Size:</b> 6,7,8,9,10<br>
       <b>Material:</b> Leather
     `
-  },
-
-  {
-    id: "shoe-2",
-    category: "shoes",
-    title: "Olive Suede Rowan Leather",
-    price: "₹3,599",
-    short: "",
-    search: "olive suede rowan leather shoes",
-    page: "shoes.html",
-    images: ["shoe2-1.jpg","shoe2-2.jpg"],
-    description: ``
   }
 ];
