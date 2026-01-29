ec420ec5446cf2a5336cee3bc5db22ee177a72d7
One Love <royaltrendzindia@gmail.com>
2026-01-29 06:55:14 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,22 @@
+const PRODUCTS = {
+  "jacket-1": {
+    category: "clothing",
+    title: "Campus Sutra Zip-Front Bomber Jacket",
+    price: 1999,
+    images: ["jacket1-1.jpg","jacket1-2.jpg","jacket1-3.jpg"],
+    details: {
+      size: "S, M, L, XL",
+      fit: "Regular Fit",
+      care: "Machine Wash",
+      origin: "India"
+    }
+  },
+
+  "shoe-1": {
+    category: "shoes",
+    title: "Men’s Brown Suede Sneakers",
+    price: null,
+    stock: "out",
+    images: ["shoe1-1.jpg","shoe1-2.jpg","shoe1-3.jpg"]
+  }
+};
