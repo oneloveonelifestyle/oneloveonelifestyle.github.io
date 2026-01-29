505a7a8ac2243110c6344b018b4d62350e762313
One Love <royaltrendzindia@gmail.com>
2026-01-29 07:17:39 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,22 +1,64 @@
-const PRODUCTS = {
-  "jacket-1": {
-    category: "clothing",
+const PRODUCTS = [
+  {
+    id: "jacket-1",
+    page: "clothing",
+    search: "campus sutra jacket bomber regular",
     title: "Campus Sutra Zip-Front Bomber Jacket",
-    price: 1999,
-    images: ["jacket1-1.jpg","jacket1-2.jpg","jacket1-3.jpg"],
-    details: {
-      size: "S, M, L, XL",
-      fit: "Regular Fit",
-      care: "Machine Wash",
-      origin: "India"
-    }
+    price: "₹1,999",
+    short: "Regular Fit",
+    images: ["jacket1-1.jpg", "jacket1-2.jpg", "jacket1-3.jpg"],
+    description: `
+      <b>Size :</b> S, M, L, XL<br><br>
+      <b>Fit:</b> Regular Fit<br>
+      <b>Care Instruction:</b> Machine Wash
+    `
   },
-
-  "shoe-1": {
-    category: "shoes",
-    title: "Men’s Brown Suede Sneakers",
-    price: null,
-    stock: "out",
-    images: ["shoe1-1.jpg","shoe1-2.jpg","shoe1-3.jpg"]
+  {
+    id: "jacket-2",
+    page: "clothing",
+    search: "nuon jacket cotton relaxed brown",
+    title: "Nuon Brown Text Design Relaxed-Fit Cotton Jacket",
+    price: "₹2,999",
+    short: "Relaxed Fit",
+    images: ["shirt1-3.jpg", "shirt1-4.jpg", "shirt1-5.jpg"],
+    description: `
+      <b>Size :</b> XS, S, M, L, XL<br><br>
+      <b>Fit:</b> Relaxed Fit<br>
+      <b>Care Instruction:</b> Machine Wash<br>
+      <b>Fabric Composition:</b> 100% Cotton<br><br>
+      <b>Country Of Origin:</b> India
+    `
+  },
+  {
+    id: "shirt-1",
+    page: "clothing",
+    search: "eta shirt cotton brown relaxed",
+    title: "ETA Brown Textured Relaxed-Fit Cotton Shirt",
+    price: "₹1,799",
+    short: "Relaxed Fit",
+    images: ["shirt1-1.jpg", "shirt1-2.jpg"],
+    description: `
+      <b>Size :</b> XS, S, M, L<br><br>
+      <b>Fit:</b> Relaxed Fit<br>
+      <b>Care Instruction:</b> Machine Wash<br>
+      <b>Fabric Composition:</b> 100% Cotton<br><br>
+      <b>Country Of Origin:</b> India
+    `
+  },
+  {
+    id: "jacket-4",
+    page: "clothing",
+    search: "studiofit dark brown hooded jacket relaxed",
+    title: "Studiofit Dark Brown Relaxed-Fit Hooded Jacket",
+    price: "₹2,199",
+    short: "Relaxed Fit",
+    images: ["jacket3-1.jpg", "jacket3-2.jpg", "jacket3-3.jpg"],
+    description: `
+      <b>Size :</b> XS, S, M, L<br><br>
+      <b>Fit:</b> Relaxed Fit<br>
+      <b>Care Instruction:</b> Machine Wash<br>
+      <b>Fabric Composition:</b> 79% Polyester, 18% Viscose, 3% Elastane<br><br>
+      <b>Country Of Origin:</b> India
+    `
   }
-};
+];
