2c7cbd009b939efd75883beefea0c58c3c56f54b
One Love <royaltrendzindia@gmail.com>
2026-01-29 07:43:49 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,12 +1,11 @@
 const PRODUCTS = [
-  /* ========= CLOTHING ========= */
   {
     id: "jacket-1",
     category: "clothing",
+    search: "campus sutra jacket bomber regular",
     title: "Campus Sutra Zip-Front Bomber Jacket",
     price: "₹1,999",
     short: "Regular Fit",
-    search: "campus sutra jacket bomber regular",
     images: ["jacket1-1.jpg","jacket1-2.jpg","jacket1-3.jpg"],
     description: `
       <b>Size :</b> S, M, L, XL<br><br>
@@ -17,56 +16,48 @@ const PRODUCTS = [
   {
     id: "jacket-2",
     category: "clothing",
+    search: "nuon jacket cotton relaxed brown",
     title: "Nuon Brown Text Design Relaxed-Fit Cotton Jacket",
     price: "₹2,999",
     short: "Relaxed Fit",
-    search: "nuon jacket cotton relaxed brown",
     images: ["shirt1-3.jpg","shirt1-4.jpg","shirt1-5.jpg"],
     description: `
       <b>Size :</b> XS, S, M, L, XL<br><br>
       <b>Fit:</b> Relaxed Fit<br>
-      <b>Fabric:</b> 100% Cotton
+      <b>Care Instruction:</b> Machine Wash<br>
+      <b>Fabric Composition:</b> 100% Cotton<br><br>
+      <b>Country Of Origin:</b> India
     `
   },
   {
     id: "shirt-1",
     category: "clothing",
+    search: "eta shirt cotton brown relaxed",
     title: "ETA Brown Textured Relaxed-Fit Cotton Shirt",
     price: "₹1,799",
     short: "Relaxed Fit",
-    search: "eta shirt cotton brown relaxed",
     images: ["shirt1-1.jpg","shirt1-2.jpg"],
     description: `
       <b>Size :</b> XS, S, M, L<br><br>
-      <b>Fabric:</b> 100% Cotton
+      <b>Fit:</b> Relaxed Fit<br>
+      <b>Care Instruction:</b> Machine Wash<br>
+      <b>Fabric Composition:</b> 100% Cotton<br><br>
+      <b>Country Of Origin:</b> India
     `
   },
   {
     id: "jacket-4",
     category: "clothing",
+    search: "studiofit dark brown hooded jacket relaxed",
     title: "Studiofit Dark Brown Relaxed-Fit Hooded Jacket",
     price: "₹2,199",
     short: "Relaxed Fit",
-    search: "studiofit dark brown hooded jacket relaxed",
     images: ["jacket3-1.jpg","jacket3-2.jpg","jacket3-3.jpg"],
     description: `
       <b>Size :</b> XS, S, M, L<br><br>
-      <b>Fabric:</b> Polyester Blend
-    `
-  },
-
-  /* ========= SHOES ========= */
-  {
-    id: "shoe-1",
-    category: "shoes",
-    title: "Men’s Brown Suede Sneakers",
-    price: "Currently Out of Stock",
-    short: "",
-    search: "mens brown suede sneakers leather",
-    images: ["shoe1-1.jpg","shoe1-2.jpg","shoe1-3.jpg"],
-    description: `
-      <b>Size:</b> 6,7,8,9,10<br>
-      <b>Material:</b> Leather
+      <b>Fit:</b> Relaxed Fit<br>
+      <b>Care Instruction:</b> Machine Wash<br>
+      <b>Country Of Origin:</b> India
     `
   }
 ];
