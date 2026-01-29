138345e7ffa799066e5fa9fbf231191887429192
One Love <royaltrendzindia@gmail.com>
2026-01-29 08:24:56 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,12 +1,15 @@
+/* ================================
+   CENTRAL PRODUCT DATABASE
+   Do NOT put HTML here
+   Used by clothing.html, shoes.html, vault.html
+================================ */
+
 const PRODUCTS = [
-  /* ===============================
-     CLOTHING
-     =============================== */
+  /* ================= CLOTHING ================= */
 
   {
     id: "jacket-1",
     category: "clothing",
-    page: "clothing.html",
     title: "Campus Sutra Zip-Front Bomber Jacket",
     price: "₹1,999",
     short: "Regular Fit",
@@ -22,7 +25,6 @@ const PRODUCTS = [
   {
     id: "jacket-2",
     category: "clothing",
-    page: "clothing.html",
     title: "Nuon Brown Text Design Relaxed-Fit Cotton Jacket",
     price: "₹2,999",
     short: "Relaxed Fit",
@@ -40,7 +42,6 @@ const PRODUCTS = [
   {
     id: "shirt-1",
     category: "clothing",
-    page: "clothing.html",
     title: "ETA Brown Textured Relaxed-Fit Cotton Shirt",
     price: "₹1,799",
     short: "Relaxed Fit",
@@ -50,15 +51,13 @@ const PRODUCTS = [
       <b>Size :</b> XS, S, M, L<br><br>
       <b>Fit:</b> Relaxed Fit<br>
       <b>Care Instruction:</b> Machine Wash<br>
-      <b>Fabric Composition:</b> 100% Cotton<br><br>
-      <b>Country Of Origin:</b> India
+      <b>Fabric Composition:</b> 100% Cotton
     `
   },
 
   {
     id: "jacket-4",
     category: "clothing",
-    page: "clothing.html",
     title: "Studiofit Dark Brown Relaxed-Fit Hooded Jacket",
     price: "₹2,199",
     short: "Relaxed Fit",
@@ -67,27 +66,24 @@ const PRODUCTS = [
     description: `
       <b>Size :</b> XS, S, M, L<br><br>
       <b>Fit:</b> Relaxed Fit<br>
-      <b>Care Instruction:</b> Machine Wash<br>
-      <b>Country Of Origin:</b> India
+      <b>Care Instruction:</b> Machine Wash
     `
   },
 
-  /* ===============================
-     SHOES
-     =============================== */
+  /* ================= SHOES ================= */
 
   {
     id: "shoe-1",
     category: "shoes",
-    page: "shoes.html",
     title: "Men’s Brown Suede Sneakers",
     price: "Currently Out of Stock",
-    short: "Leather Sneakers",
+    short: "Leather Upper",
     search: "mens brown suede sneakers leather",
     images: ["shoe1-1.jpg", "shoe1-2.jpg", "shoe1-3.jpg"],
     description: `
-      <b>Size:</b> 6, 7, 8, 9, 10<br>
+      <b>Size:</b> 6, 7, 8, 9, 10<br><br>
       <b>Material:</b> Leather
     `
   }
+
 ];
