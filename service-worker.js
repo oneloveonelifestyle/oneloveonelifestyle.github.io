2166c9b339061938dc0a4233ae82bea4cfda32e4
One Love <royaltrendzindia@gmail.com>
2026-01-29 12:22:01 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,5 @@
-self.addEventListener("install", event => {
+self.addEventListener("install", e => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
-});
+self.addEventListener("fetch", () => {});
