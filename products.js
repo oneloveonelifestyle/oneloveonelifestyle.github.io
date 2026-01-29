1bcc443b80fd950455da2d0ae9a5d7a410034cc6
One Love <royaltrendzindia@gmail.com>
2026-01-29 08:56:01 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,5 +1,6 @@
 const PRODUCTS = [
-  /* ================= CLOTHING ================= */
+
+  /* ======================= CLOTHING ======================= */
 
   {
     id: "jacket-1",
@@ -7,30 +8,70 @@ const PRODUCTS = [
     title: "Campus Sutra Zip-Front Bomber Jacket",
     price: "₹1,999",
     stock: true,
-    search: "campus sutra bomber jacket",
+    short: "Regular Fit",
+    search: "campus sutra jacket bomber regular",
     images: ["jacket1-1.jpg", "jacket1-2.jpg", "jacket1-3.jpg"],
     description: `
-      <b>Size:</b> S, M, L, XL<br>
-      <b>Fit:</b> Regular Fit<br>
-      <b>Care:</b> Machine Wash
+      <b>Size :</b> S, M, L, XL<br><br>
+      <b>Fit :</b> Regular Fit<br>
+      <b>Care Instruction :</b> Machine Wash
     `
   },
 
   {
     id: "jacket-2",
     category: "clothing",
-    title: "Nuon Brown Relaxed-Fit Cotton Jacket",
-    price: "₹2,199",
+    title: "Nuon Brown Text Design Relaxed-Fit Cotton Jacket",
+    price: "₹2,999",
     stock: true,
-    search: "nuon brown cotton jacket",
+    short: "Relaxed Fit",
+    search: "nuon jacket cotton relaxed brown",
     images: ["shirt1-3.jpg", "shirt1-4.jpg", "shirt1-5.jpg"],
     description: `
-      <b>Size:</b> XS, S, M, L, XL<br>
-      <b>Fit:</b> Relaxed Fit
+      <b>Size :</b> XS, S, M, L, XL<br><br>
+      <b>Fit :</b> Relaxed Fit<br>
+      <b>Care Instruction :</b> Machine Wash<br>
+      <b>Fabric :</b> 100% Cotton<br><br>
+      <b>Country Of Origin :</b> India
     `
   },
 
-  /* ================= SHOES ================= */
+  {
+    id: "shirt-1",
+    category: "clothing",
+    title: "ETA Brown Textured Relaxed-Fit Cotton Shirt",
+    price: "₹1,799",
+    stock: true,
+    short: "Relaxed Fit",
+    search: "eta shirt cotton brown relaxed",
+    images: ["shirt1-1.jpg", "shirt1-2.jpg"],
+    description: `
+      <b>Size :</b> XS, S, M, L<br><br>
+      <b>Fit :</b> Relaxed Fit<br>
+      <b>Care Instruction :</b> Machine Wash<br>
+      <b>Fabric :</b> 100% Cotton<br><br>
+      ETA redefines everyday comfort with this cotton-rich shirt.
+    `
+  },
+
+  {
+    id: "jacket-4",
+    category: "clothing",
+    title: "Studiofit Dark Brown Relaxed-Fit Hooded Jacket",
+    price: "₹2,199",
+    stock: true,
+    short: "Relaxed Fit",
+    search: "studiofit dark brown hooded jacket relaxed",
+    images: ["jacket3-1.jpg", "jacket3-2.jpg", "jacket3-3.jpg"],
+    description: `
+      <b>Size :</b> XS, S, M, L<br><br>
+      <b>Fit :</b> Relaxed Fit<br>
+      <b>Care Instruction :</b> Machine Wash<br>
+      <b>Fabric :</b> Polyester Blend
+    `
+  },
+
+  /* ======================= SHOES ======================= */
 
   {
     id: "shoe-1",
@@ -38,11 +79,12 @@ const PRODUCTS = [
     title: "Men’s Brown Suede Sneakers",
     price: "Currently Out of Stock",
     stock: false,
-    search: "brown suede sneakers men",
+    short: "Suede Leather",
+    search: "mens brown suede sneakers leather",
     images: ["shoe1-1.jpg", "shoe1-2.jpg", "shoe1-3.jpg"],
     description: `
-      <b>Size:</b> 6, 7, 8, 9, 10<br>
-      <b>Material:</b> Suede Leather
+      <b>Size :</b> 6, 7, 8, 9, 10<br>
+      <b>Material :</b> Suede Leather
     `
   },
 
@@ -52,11 +94,14 @@ const PRODUCTS = [
     title: "Olive Suede Rowan Leather",
     price: "₹3,599",
     stock: true,
+    short: "Premium Leather",
     search: "olive suede rowan leather shoes",
     images: ["shoe2-1.jpg", "shoe2-2.jpg"],
     description: `
-      <b>Size:</b> 7, 8, 9, 10<br>
-      <b>Material:</b> Leather
+      <b>Size :</b> 7, 8, 9, 10<br>
+      <b>Material :</b> Leather<br>
+      <b>Sole :</b> Rubber
     `
   }
+
 ];
