124bf3c1e69674fbe54c923f52ccfdb3d8386ee2
One Love <royaltrendzindia@gmail.com>
2026-01-29 09:00:43 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,20 +1,22 @@
-const PRODUCTS = [
+/* =========================
+   MASTER PRODUCT DATABASE
+   ========================= */
 
-  /* ======================= CLOTHING ======================= */
+const PRODUCTS = [
+  /* ================= CLOTHING ================= */
 
   {
     id: "jacket-1",
     category: "clothing",
     title: "Campus Sutra Zip-Front Bomber Jacket",
     price: "₹1,999",
-    stock: true,
     short: "Regular Fit",
     search: "campus sutra jacket bomber regular",
     images: ["jacket1-1.jpg", "jacket1-2.jpg", "jacket1-3.jpg"],
     description: `
       <b>Size :</b> S, M, L, XL<br><br>
-      <b>Fit :</b> Regular Fit<br>
-      <b>Care Instruction :</b> Machine Wash
+      <b>Fit:</b> Regular Fit<br>
+      <b>Care Instruction:</b> Machine Wash
     `
   },
 
@@ -23,16 +25,15 @@ const PRODUCTS = [
     category: "clothing",
     title: "Nuon Brown Text Design Relaxed-Fit Cotton Jacket",
     price: "₹2,999",
-    stock: true,
     short: "Relaxed Fit",
     search: "nuon jacket cotton relaxed brown",
     images: ["shirt1-3.jpg", "shirt1-4.jpg", "shirt1-5.jpg"],
     description: `
       <b>Size :</b> XS, S, M, L, XL<br><br>
-      <b>Fit :</b> Relaxed Fit<br>
-      <b>Care Instruction :</b> Machine Wash<br>
-      <b>Fabric :</b> 100% Cotton<br><br>
-      <b>Country Of Origin :</b> India
+      <b>Fit:</b> Relaxed Fit<br>
+      <b>Care Instruction:</b> Machine Wash<br>
+      <b>Fabric Composition:</b> 100% Cotton<br><br>
+      <b>Country Of Origin:</b> India
     `
   },
 
@@ -41,16 +42,14 @@ const PRODUCTS = [
     category: "clothing",
     title: "ETA Brown Textured Relaxed-Fit Cotton Shirt",
     price: "₹1,799",
-    stock: true,
     short: "Relaxed Fit",
     search: "eta shirt cotton brown relaxed",
     images: ["shirt1-1.jpg", "shirt1-2.jpg"],
     description: `
       <b>Size :</b> XS, S, M, L<br><br>
-      <b>Fit :</b> Relaxed Fit<br>
-      <b>Care Instruction :</b> Machine Wash<br>
-      <b>Fabric :</b> 100% Cotton<br><br>
-      ETA redefines everyday comfort with this cotton-rich shirt.
+      <b>Fit:</b> Relaxed Fit<br>
+      <b>Care Instruction:</b> Machine Wash<br>
+      <b>Fabric Composition:</b> 100% Cotton
     `
   },
 
@@ -59,32 +58,30 @@ const PRODUCTS = [
     category: "clothing",
     title: "Studiofit Dark Brown Relaxed-Fit Hooded Jacket",
     price: "₹2,199",
-    stock: true,
     short: "Relaxed Fit",
     search: "studiofit dark brown hooded jacket relaxed",
     images: ["jacket3-1.jpg", "jacket3-2.jpg", "jacket3-3.jpg"],
     description: `
       <b>Size :</b> XS, S, M, L<br><br>
-      <b>Fit :</b> Relaxed Fit<br>
-      <b>Care Instruction :</b> Machine Wash<br>
-      <b>Fabric :</b> Polyester Blend
+      <b>Fit:</b> Relaxed Fit<br>
+      <b>Care Instruction:</b> Machine Wash
     `
   },
 
-  /* ======================= SHOES ======================= */
+  /* ================= SHOES ================= */
 
   {
     id: "shoe-1",
     category: "shoes",
     title: "Men’s Brown Suede Sneakers",
     price: "Currently Out of Stock",
-    stock: false,
-    short: "Suede Leather",
+    short: "Leather Upper",
     search: "mens brown suede sneakers leather",
+    outOfStock: true,
     images: ["shoe1-1.jpg", "shoe1-2.jpg", "shoe1-3.jpg"],
     description: `
-      <b>Size :</b> 6, 7, 8, 9, 10<br>
-      <b>Material :</b> Suede Leather
+      <b>Size:</b> 6, 7, 8, 9, 10<br>
+      <b>Material:</b> Leather
     `
   },
 
@@ -93,15 +90,13 @@ const PRODUCTS = [
     category: "shoes",
     title: "Olive Suede Rowan Leather",
     price: "₹3,599",
-    stock: true,
     short: "Premium Leather",
     search: "olive suede rowan leather shoes",
+    outOfStock: false,
     images: ["shoe2-1.jpg", "shoe2-2.jpg"],
     description: `
-      <b>Size :</b> 7, 8, 9, 10<br>
-      <b>Material :</b> Leather<br>
-      <b>Sole :</b> Rubber
+      <b>Material:</b> Suede Leather<br>
+      <b>Color:</b> Olive
     `
   }
-
 ];
